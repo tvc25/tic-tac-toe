2940a1b16ec323f85e1b1f80f9a44e8e3f1b84cc
Tobia Cassandro <tobiacassandro@gmail.com>
2015-05-12 12:46:17 +0100

    made some great CSS moves

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var playerOne = 'X'
+var playerTwo = 'O'
+var winningCombo = [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7']]
+
+// for (var i = 0; i >= winningCombo.length; i+) {
+//   console.log(winningCombo[i]);
+// };
+
+// 
+
+$(document).ready(function(){
+  $('.box').on('click', function(e){
+    $(this).addClass('nought');
+  })
+
+
+
+
+
+
+})
